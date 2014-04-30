61206f1b813e77dfe5e69ea2c9649c78058d28ec
XHAGBARD <ximop_22@hotmail.com>
2014-04-30 10:12:54 +0200

    Create csadduser.sh

---
+++ source/csadduser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#csadduser.sh
+#
+#Script para añadir usuarios y configurarlos en el servidor automaticamente.
+#
+#Extraer variables
+mes=$[$(date +%m)+1]
+#Creamos el usuario con sus opciones
+useradd -d /home/$1 -e $(date +%d/$mes/%Y) -g 1221 -m -s /bin/false $1
+echo "El usuario $1 ha sido creado correctamente."
+passwd $1
+echo "La contraseña se ha guardado correctamente"
+#Añadimos al usuario en Samba
+smbpasswd -a $1
+#Habilitamos las quotas en /home para el Cloud
+setquota -u $1 1457280 30000000 0 0 /home
