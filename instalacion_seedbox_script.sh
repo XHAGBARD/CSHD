7111754cdddbb603f831e142fa05abb775d75961
XHAGBARD <ximop_22@hotmail.com>
2014-05-04 18:00:48 +0000

    Update instalacion_seedbox_script.sh

--- instalacion_seedbox_script.sh
+++ instalacion_seedbox_script.sh
@@ -60,7 +60,7 @@ apt-get update
 apt-get -y upgrade
 
 #Instalacion de los paquetes necesarios para el servidor#
-apt-get install apache2 php5 php5-curl libapache2-mod-php5 mysql-client mysql-server phpmyadmin sendmail vsftpd lftp samba samba-common smbfs smbclient openvpn openssl openssh-server zip quota webmin whois sudo makepasswd git
+apt-get install -y apache2 php5 php5-curl libapache2-mod-php5 mysql-client mysql-server phpmyadmin sendmail vsftpd lftp samba samba-common smbfs smbclient openvpn openssl openssh-server zip quota whois sudo makepasswd
 
 if [ $? -gt 0 ]; then
         echo
